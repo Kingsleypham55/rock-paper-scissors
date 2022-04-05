cdc61e409a16c0c21b799b156a7b08dc4adf16af
Kingsley Pham <kingsleypham55@gmail.com>
2022-04-05 13:52:58 -0500

    ADD style.css, and Updated Index.html and script.js

--- script.js
+++ script.js
@@ -1,74 +1,98 @@
-const option = ['rock', 'paper','scissor']
-
-   
+const option = ['Rock', 'Paper','Scissor']
+const text = document.querySelector("#text")
+let playerScore = parseInt(document.querySelector('#playerScore').innerText);
+let computerScore = parseInt(document.querySelector('#computerScore').innerText);   
 function computerPlay(){
 let computer = option[Math.floor(Math.random()*option.length)];
 return computer    
 }
+const rockButton = document.querySelector('#rockButton')
+rockButton.addEventListener("click", function(){
+    playerInput=('Rock')
+    game()
+})
+const paperButton = document.querySelector('#paperButton')
+paperButton.addEventListener("click", function(){
+    playerInput=('Paper')
+    game()
+})
+const scissorButton = document.querySelector('#scissorButton')
+scissorButton.addEventListener("click", function(){
+    playerInput=('Scissor')
+    game()
+})
+
+const resetGame = document.querySelector('#resetButton')
+resetGame.addEventListener("click", function(){
+    playerScore = 0
+    computerScore = 0
+    document.querySelector('#playerScore').innerText = playerScore;
+    document.querySelector('#computerScore').innerText = computerScore;
+    text.textContent = 'First to 5 Point Win'
+})
 function playRound(playerSelection, computerSelection){ 
-var playerSelection = prompt('Rock, Paper, or Scissor?').toLowerCase();
+var playerSelection = playerInput;
 var computerSelection = computerPlay()
  
-    if(playerSelection === 'rock'){
+    if(playerSelection === 'Rock'){
         switch (computerSelection){
-            case 'rock':
+            case 'Rock':
                 return "Tie"
-            case 'paper':
+            case 'Paper':
                 return "Lose"
-            case 'scissor':
+            case 'Scissor':
                 return "Win"
         }
-    }else if(playerSelection === 'paper'){
+    }else if(playerSelection === 'Paper'){
         switch (computerSelection){
-            case 'rock':
+            case 'Rock':
                 return "Win"
-            case 'paper':
+            case 'Paper':
                 return "Tie"
-            case 'scissor':
+            case 'Scissor':
                 return "Lose"
         }
-    }else if(playerSelection === 'scissor'){
+    }else if(playerSelection === 'Scissor'){
         switch (computerSelection){
-            case 'rock':
+            case 'Rock':
                 return "Lose"
-            case 'paper':
+            case 'Paper':
                 return "Win"
-            case 'scissor':
+            case 'Scissor':
                 return "Tie"
         }
     }
 }
 function game(){
+    var playerSelection = playerInput;
+    var computerSelection = computerPlay()
     const lose = "You Lose!";
     const win = "You Win!";
     const tie = "It's a tie";
-    let playerScore = 0;
-    let computerScore = 0;
-    for (let i = 0; i < 5; i++) {
+    for (let i = 0; i < 1; i++) {
         const result = playRound()
         switch (result){
             case 'Win':
-                console.log(win);
+                console.log(win,playerSelection,computerSelection);
+                text.textContent = `Player Win.`
                 playerScore++
+                document.querySelector('#playerScore').innerText = playerScore;
                 break;
             case 'Lose':
-                console.log(lose);
+                console.log(lose,playerSelection,computerSelection);
+                text.textContent = `Computer Win. `
                 computerScore++
+                document.querySelector('#computerScore').innerText = computerScore;
                 break;
             case 'Tie':
-                console.log(tie);
-                playerScore++
-                computerScore++
+                text.textContent = `It A Tie. Both Picked ${playerSelection}`
+                console.log(tie,playerSelection,computerSelection);
                 break;
         }
 }
-console.log("Final Result: Player: "+ playerScore + " Computer: " + computerScore);
-if (playerScore > computerScore){
-    console.log("You Win The Game .")
-} else if ( playerScore < computerScore){
-    console.log("Computer win The Game.")
-}else {
-    console.log("The Game Was A tie.")
+if (playerScore == 5 || computerScore == 5){
+    alert("Player Win, Computer Lose. Click Reset To Play Again.")
+    text.textContent = 'Game Over'
 }
-}
-game()
+} 
+
